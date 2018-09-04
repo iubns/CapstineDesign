3a3ab0c3868ca9d4a82f5b15510d3bc8a569767f
문수 박 <qkranstn3@naver.com>
2018-09-04 15:59:56 +0900

    test

--- surveillanceProgram/surveillanceProgram/Program.cs
+++ surveillanceProgram/surveillanceProgram/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,20 +13,16 @@ namespace surveillanceProgram
     {
         static void Main(string[] args)
         {
-            IPAddress ipAddress = IPAddress.Parse("172.17.127.54");
+            IPAddress ipAddress = IPAddress.Parse("172.17.128.17");
             IPEndPoint ipep = new IPEndPoint(ipAddress, 9001);
-            /*
+            
             Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             byte[] temp = new byte[100];
             server.Connect(ipep);
+            server.Send(new byte[] { 10 });
             server.Receive(temp);//LeagueClient
-            */
-            string[] programList = { "chrome", "Google Chrome" };
-            foreach (Process process in Process.GetProcesses())
-            {
-                Console.WriteLine(process.ToString());
-            }
-            Console.ReadLine();
+           
+            string[] programList = { "LeagueClient", "Google Chrome" };
             foreach (string program in programList)
             {
                 for (int i = 0; i < 20; i++)
