c5521d71ff68cdcbfa4047967c2abbc91a6805e7
iubns <iubns@naver.com>
2018-09-04 15:27:56 +0900

    IP변경 및 자신이 누군지 알리기 추가

--- surveillanceProgram/surveillanceProgram/Program.cs
+++ surveillanceProgram/surveillanceProgram/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,20 +13,22 @@ namespace surveillanceProgram
     {
         static void Main(string[] args)
         {
-            IPAddress ipAddress = IPAddress.Parse("172.17.127.54");
+            IPAddress ipAddress = IPAddress.Parse("172.17.128.17");
             IPEndPoint ipep = new IPEndPoint(ipAddress, 9001);
-            /*
+
             Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             byte[] temp = new byte[100];
             server.Connect(ipep);
-            server.Receive(temp);//LeagueClient
-            */
-            string[] programList = { "chrome", "Google Chrome" };
+            server.Send(Encoding.UTF8.GetBytes("student"));
+            server.Receive(temp);
+            /*
             foreach (Process process in Process.GetProcesses())
             {
                 Console.WriteLine(process.ToString());
             }
             Console.ReadLine();
+            */
+            string[] programList = { "LeagueClient" };
             foreach (string program in programList)
             {
                 for (int i = 0; i < 20; i++)
