6ce29f7bb11655c4b662ec6d581aafa90ad890d7
iubns <iubns@naver.com>
2018-10-22 16:21:09 +0900

    fixed gameSearcing

--- ComputerControl/ComputerControl/MainWindow.xaml.cs
+++ ComputerControl/ComputerControl/MainWindow.xaml.cs
@@ -44,7 +44,7 @@ namespace ComputerControl
 
         private void CheckVersion()
         {
-            if (WebConnection.GetVersion() != "1.0.0")
+            if (WebConnection.GetVersion() != "1.0.1")
             {
                 WebConnection.GetUpdate();
             }
